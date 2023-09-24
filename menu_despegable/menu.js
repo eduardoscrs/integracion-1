284dc4eb7fb797042104c5968161e78eea2b0182
NicolasVZ86 <nvalenzuela2022@alu.uct.cl>
2023-09-23 22:03:18 -0300

    ordenar archivosv1

--- menu_despegable/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.querySelector(".menu button");
-    const menuNav = document.querySelector(".menu-nav");
-
-    menuButton.addEventListener("click", function() {
-        menuNav.classList.toggle("show-menu");
-    });
-});
